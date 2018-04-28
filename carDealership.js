37d45cd9df38611d184f4e237d3c548cb8ec8994
Chris Milkaitis <chris.milkaitis@gmail.com>
2018-04-28 11:54:39 -0400

    added variables and console.log for request results

--- carDealership.js
+++ carDealership.js
@@ -6,7 +6,7 @@ var carDealership = {
         numAvailable: 20,
         request : function(){
             if (carDealership.sedan.available === 'yes'){
-                return `There are ${carDealership.sedan.numAvailable} sedans available today at ${carDealership.sedan.price}.`
+                return `There are ${carDealership.sedan.numAvailable} sedans available today at ${carDealership.sedan.price}.`;
             }
             return `There are no cars available today`;
         }
@@ -18,7 +18,7 @@ var carDealership = {
         numAvailable: 15,
         request : function(){
             if (carDealership.suv.available === 'yes'){
-                return `There are ${carDealership.suv.numAvailable} SUVs available today at ${carDealership.suv.price}.`
+                return `There are ${carDealership.suv.numAvailable} SUVs available today at ${carDealership.suv.price}.`;
             }
             return `There are no cars available today`;
         }
@@ -31,10 +31,21 @@ var carDealership = {
         numAvailable: 0,
         request : function(){
             if (carDealership.sports.available === 'yes'){
-                return `There are ${carDealership.sports.numAvailable} sports cars available today at ${carDealership.sports.price}.`
+                return `There are ${carDealership.sports.numAvailable} sports cars available today at ${carDealership.sports.price}.`;
             }
             return `There are no cars available today`;
         }
     }
 
 }
+
+var request_sedan = carDealership.sedan.request();
+var request_sportsCar = carDealership.sports.request();
+var request_SUV = carDealership.suv.request();
+
+
+console.log(request_sedan);
+console.log(request_sportsCar);
+console.log(request_SUV);
+
+
